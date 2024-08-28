daa3bbcdb454b53f02db7ab0c25542c8371d7f69
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2024-08-28 20:04:14 +0200

    Pass file path as command line argument

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
+use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let file = File::open("non_existent_file.txt");
+    // Collect command line arguments
+    let args: Vec<String> = env::args().collect();
+    // The first argument is the path that was used to call the program
+    println!("Executable path is {}.", args[0]);
+    let file_path= &args[1];
+
+    // Open file
+    let file = File::open(file_path);
     let file = match file {
         Ok(file) => file,
         Err(error) => {
@@ -17,6 +25,7 @@ fn main() {
         }
     };
 
+    // Read file
     let reader = BufReader::new(file);
     for line in reader.lines() {
         match line {
